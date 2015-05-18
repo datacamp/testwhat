1a45a43450afe1eee94990500084c01dcde83e15
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-05-18 13:37:48 +0200

    not_found_msg can be manually specified

--- inst/offline_testwhat_testing.R
+++ inst/offline_testwhat_testing.R
@@ -25,27 +25,23 @@ get_output <- function(code) {
 ####################################
 
 assign('pre_exercise_code', '
-	train <- read.csv(url("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"))
-test <- read.csv(url("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"))
-library(rpart)
+
 ',env=globalenv())
 
 
 #SOLUTION CODE
 set_solution_code('
 
-# Variables related to your last day of recordings
 medium <- "LinkedIn"
 num_views <- 14
 
-# Examine the if statement for medium
+# Control structure for medium
 if(medium == "LinkedIn") {
   print("Showing LinkedIn information")
-}
-
-# Write the if statement for num_views
-if(num_views > 15) {
-  print("You\'re popular!")
+} else if(medium == "Facebook") {
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
 }
 
 ')
@@ -53,46 +49,49 @@ if(num_views > 15) {
 # USER CODE
 set_student_code('
 
-# Variables related to your last day of recordings
 medium <- "LinkedIn"
 num_views <- 14
 
-# Examine the if statement for medium
+# Control structure for medium
 if(medium == "LinkedIn") {
   print("Showing LinkedIn information")
+} else if(medium == "Facebook") {
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
 }
-
-# Write the if statement for num_views
-if(num_views > 15) {
   
-}
 ')
 
 sct = '
 
 test_error()
-
 test_if_else(index = 1, 
-if_cond_test = {
-test_student_typed(c("medium == \\"LinkedIn\\"", "\\"LinkedIn\\" == medium"),
-not_typed_msg = "Have another look at the <code>condition</code> part of the first <code>if</code> statement. It was already coded for you!")
-},
+             if_cond_test = {
+               test_student_typed(c("medium == \\"LinkedIn\\"", "\\"LinkedIn\\" == medium"),
+                                  not_typed_msg = paste("Don\'t change anything about the <code>if</code> part of the first control structure,",
+                                                        "it has already been coded for you."))
+             },
+             if_expr_test = {
+               test_if_else(index = 1,
+                            if_cond_test = {
+                              test_student_typed(c("medium == \\"Facebook\\"", "\\"Facebook\\" == medium"),
+not_typed_msg = paste("Make sure to code the correct condition for the <code>if else</code>", 
+                      "part of the control structure."))
+
+}, 
 if_expr_test = {
-msg <- "Don\'t change anything about the printout of the message in the first <code>if</code> statement. It has already been coded for you!"
-test_function("print", "x", not_called_msg = msg, incorrect_msg = msg)
+  msg <- paste("In the <code>if else</code> part of the first control construct, make sure to print", 
+               "<code>\\"Showing Facebook Information\\"</code> using the <code>print()</code> function.")
+  test_function("print", "x", not_called_msg = msg, incorrect_msg = msg)
+},
+else_expr_test = {
+  msg <- "Don\'t change anything about the <code>else</code> part of the first control structure."
+  test_function("print", "x", not_called_msg = msg, incorrect_msg = msg)
+})
 })
 
-test_if_else(index = 2,
-if_cond_test = {
-test_student_typed(c("num_views > 15", "15 < num_views"),
-not_typed_msg = "Have another look at the <code>condition</code> part of your second <code>if</code> statement. You should use <code>num_views</code>!")
-},
-if_expr_test = {
-msg <- "Use the function <code>print()</code> to print the message <code>\\"You\'re popular!\\"</code> when the number of views exceeds 15."
-test_function("print", "x", not_called_msg = msg, incorrect_msg = msg)
-})
-
-success_msg("Great! Try to see what happens if you change the <code>medium</code> and <code>num_views</code> variables and run your code again. Let\'s further customize these if statements in the next exercise.")
+success_msg("Great!")
 '
 
 
