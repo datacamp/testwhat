3bc9ff06a57cb273e2b5ebc67f84f06307afd61f
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-05-20 16:36:31 +0200

    add first version of test_function_definition

--- R/test-function-definition.R
+++ R/test-function-definition.R
@@ -1,9 +1,63 @@
 #' Check whether the student defined a function correctly
 #' 
 #' @export
-test_function_definition <- function(name, in_and_out, student_code, student_env, undefined_msg, incorrect_msg) {
+test_function_definition <- function(name, 
+                                     function_tests = NULL, 
+                                     arguments = c(),
+                                     check_defaults = TRUE,
+                                     body_test = NULL,
+                                     student_env = .GlobalEnv,
+                                     solution_env = get_solution_env(),
+                                     student_code = get_student_code(), 
+                                     solution_code = get_solution_code(),
+                                     undefined_msg = NULL, 
+                                     incorrect_arguments_msg = NULL,
+                                     env = parent.frame()) {
   
-  test_that("write tests here", {
+  body_test <- substitute(body_test)
+  if (is.character(body_test)) code <- parse(text = body_test)
+  
+  if (is.null(undefined_msg)) {
+    undefined_msg <- sprintf("Did you define the function <code>%s()</code>?", name)
+  }
+  
+  test_that("Function is defined", {
+    expect_that(name, is_defined(env = student_env), failure_msg = undefined_msg)
+  })
+  
+  test_correct({
+    # Perform the tests on the function in the student environment
+    test_that("Function works as expected", {
+      eval(function_tests, envir = student_env)  
+    })
+  }, {
+    # if not correct, go into more detail
+    stud_function <- get(name, envir = student_env, inherits = FALSE)
+    sol_function <- get(name, envir = solution_env, inherits = FALSE)
     
+    # Check for correct definition of arguments
+    stud_arguments <- as.list(formals(stud_function))
+    sol_arguments <- as.list(formals(sol_function))
+    if (is.null(incorrect_arguments_msg)) {
+      incorrect_arguments_msg <- sprintf("In the definition of <code>%s()</code>, you did not correctly define the argument%s %s.", 
+                                       name, if(length(arguments) == 1) "" else "s", collapse_props(arguments))
+    }
+    test_that("arguments are correctly defined", {
+      for(argument in arguments) {
+        expect_that(argument %in% names(stud_arguments), is_true(), failure_msg = incorrect_arguments_msg)
+        if(check_defaults) {
+          expect_that(sol_arguments[[argument]], equals(stud_arguments[[argument]]), failure_msg = incorrect_arguments_msg)
+        }
+      }
+    })
+  
+    # Run SCT code for function body
+    if(!is.null(body_test)) {
+      set_student_code(paste(deparse(stud_function), collapse = "\n"))
+      set_solution_code(paste(deparse(sol_function), collapse = "\n"))
+      eval(body_test, envir = env)
+      set_student_code(student_code)
+      set_solution_code(solution_code)  
+    }
   })
 }
--- inst/offline_testwhat_testing.R
+++ inst/offline_testwhat_testing.R
@@ -32,61 +32,25 @@ assign('pre_exercise_code', '
 #SOLUTION CODE
 set_solution_code('
 
-# Initialize the speed variable
-speed <- 64
-
-# Code the while loop
-while(speed > 30) {
-  print("Slow down!")
-  speed <- speed - 7
+my_fun <- function(a, b) {
+  a + b
 }
-
-# Print out the speed variable
-speed
 
 ')
 
 # USER CODE
 set_student_code('
 
-# Initialize the speed variable
-speed <- 64
-
-# Code the while loop
-while(speed > 30) {
-  print("Slow d!")
-  speed <- speed - 6
+my_fun <- function(c = 2, b = 3) {
+  a + b + 1
 }
-
-# Print out the speed variable
-speed
   
 ')
 
 sct = '
 
-test_correct({
-  test_output_contains("invisible(lapply(rep(\\"Slow down!\\", 5), print))", 
-                       incorrect_msg = "Are you sure you coded the <code>while</code> loop correctly? It should print out <code>\\"Slow Down\\"</code> exactly five times!")
-}, {
-  test_while_loop(index = 1,
-                  cond_test = {
-                    test_student_typed(c("speed > 30", "30 < speed"), not_typed_msg = "Have another look at the condition of the <code>while</code> loop.")
-                  },
-                  expr_test = {
-                    msg <- "Make sure to use the <code>print()</code> function to output \\"Slow down!\\" in each run of the <code>while</code> loop."
-                    test_function("print", "x", not_called_msg = msg, incorrect_msg = msg)
-                    test_student_typed(strings = "asdfasdfasdf", not_typed_msg = "Make sure to update the <code>speed</code> variable correctly inside the <code>while</code> loop")
-                  })
-})
-
-
-test_object("speed", incorrect_msg = "After the <code>while</code> loop has executed, <code>speed</code> should equal 29. Make sure that the <code>speed</code> variable is initialized to 64 in the beginning.")
-test_output_contains("speed", incorrect_msg = "Make sure to print the final contents of the <code>speed</code> variable to the console.")
-success_msg("Great job! Proceed to the next exercise.")
-
-success_msg("OKOKOK!")
-
+test_function_definition(name = "my_fun", function_tests = { expect_that(my_fun(1,2), equals(3), failure_msg = "1 + 2 should be 3") },arguments = c("a","b"),check_defaults = FALSE,body_sct = test_student_typed("a + b")) 
+success_msg("OKE!")
 '
 
 ###################################
