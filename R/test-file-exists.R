c52d1304e4e62020148db843025dfc49e60b821b
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-09-11 09:40:44 +0200

    add test-file-exists

---
+++ R/test-file-exists.R
@@ -0,0 +1,15 @@
+#' Test whether a file is availabe in the working directory
+#' 
+#' @param path Path to the file you want to check
+#' @param incorrect_msg Feedback message in case the file does not exist
+#' @export
+test_file_exists <- function(path, incorrect_msg = NULL) {
+  
+  if(is.null(incorrect_msg)) {
+    incorrect_msg <- sprintf("The file \"%s\" does not appear to be in your working directory; make sure you don't delete it!", path)
+  }
+  
+  test_that("file is still there", {
+    expect_true(path %in% dir(), failure_msg = incorrect_msg)
+  })
+}
