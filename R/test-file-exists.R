f8e6f6cc130e619a379862a090eae7724ac70be3
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-09-17 14:58:31 +0200

    improvement file exists

--- R/test-file-exists.R
+++ R/test-file-exists.R
@@ -6,10 +6,10 @@
 test_file_exists <- function(path, incorrect_msg = NULL) {
   
   if(is.null(incorrect_msg)) {
-    incorrect_msg <- sprintf("The file \"%s\" does not appear to be in your working directory; make sure you don't delete it!", path)
+    incorrect_msg <- sprintf("The file <code>%s</code> does not appear to be in your working directory; make sure you don't delete it!", path)
   }
   
   test_that("file is still there", {
-    expect_true(path %in% dir(), failure_msg = incorrect_msg)
+    expect_true(file.exists(path), failure_msg = incorrect_msg)
   })
 }
