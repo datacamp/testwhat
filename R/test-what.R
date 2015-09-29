83f67f5f449150d16345b3db644b4859f8f66520
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-09-29 23:34:28 +0200

    remove vignette, clean up here and there

--- R/test-what.R
+++ R/test-what.R
@@ -15,16 +15,3 @@ test_what <- function(code, feedback_msg) {
   return(!rep$failed)
 }
 
-#' Easily write submission correctness tests
-#'
-#' This package makes it easy for teachers to test students' code submissions
-#' for R exercises, e.g., for interactive R courses on \url{www.DataCamp.com}.
-#'
-#' @docType package
-#' @name testwhat
-#'
-#' @note \pkg{testwhat} is forked from package \pkg{testthat} such that
-#' teachers can write their tests using a familiar framework.
-#'
-#' @references \url{www.DataCamp.com}
-NULL
---
+++ R/testwhat.R
@@ -0,0 +1,13 @@
+#' Easily write submission correctness tests for DataCamp
+#'
+#' This package makes it easy for teachers to test students' code submissions
+#' for R exercises, e.g., for interactive R courses on \url{www.DataCamp.com}.
+#'
+#' @docType package
+#' @name testwhat
+#'
+#' @note \pkg{testwhat} is a wrapper around \pkg{testthat} such that
+#' teachers can write their tests using a familiar framework.
+#'
+#' @references \url{www.DataCamp.com}
+NULL
