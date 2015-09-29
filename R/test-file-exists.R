e0c8836519fa9e2dc5b05a3b10cf613bf555bea5
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-09-29 18:18:44 +0200

    all functions on master also working

--- R/test-file-exists.R
+++ R/test-file-exists.R
@@ -4,7 +4,6 @@
 #' @param incorrect_msg Feedback message in case the file does not exist
 #' @export
 test_file_exists <- function(path, incorrect_msg = NULL) {
-  
   if(is.null(incorrect_msg)) {
     incorrect_msg <- sprintf("The file <code>%s</code> does not appear to be in your working directory; make sure you don't delete it!", path)
   }
