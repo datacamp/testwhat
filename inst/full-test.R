fd9715e5a73671173baa005571c1e2ebe8383eba
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-09-29 17:11:38 +0200

    make tests less working directory dependent

--- inst/full-test.R
+++ inst/full-test.R
@@ -1,18 +1,21 @@
-library(RAutomatedTesting)
-
-oldwd <- getwd()
-setwd(file.path(system.file(package = "testwhat")))
-if (file.exists("results.log")) file.remove("results.log")
-
-for (filename in dir("scenarios/")) {
-  if (substring(filename, 1, 5) == "test-" && 
-      substring(filename, nchar(filename)-1) == ".R") {
-    source(paste0("scenarios/",filename))
-    write(filename, file = "results.log", append = TRUE)
-    output <- test_all_scenarios(scen)
-    write(paste(output,collapse="\n"), file = "results.log", append = TRUE)
+test_everything <- function() {
+  oldwd <- getwd()
+  log_path <- file.path(oldwd, "results.log")
+  if (file.exists(log_path)) file.remove(log_path)
+  setwd(file.path(system.file(package = "testwhat")))
+  for (filename in dir("scenarios/")) {
+    if (substring(filename, 1, 5) == "test-" && 
+        substring(filename, nchar(filename)-1) == ".R") {
+      source(paste0("scenarios/",filename))
+      write(filename, file = "results.log", append = TRUE)
+      output <- test_all_scenarios(scen)
+      write(paste(output,collapse="\n"), file = log_path, append = TRUE)
+    }
   }
+  
+  # message("The results of the tests can be found here:\n", log_path)
+  setwd(oldwd)  
 }
 
-file.edit("results.log")
-setwd(oldwd)
+library(RAutomatedTesting)
+test_everything()
