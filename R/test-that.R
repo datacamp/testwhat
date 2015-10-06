7362ae49b6837f0c94d486b1431bec84ae52d777
Vincent Vankrunkelsven <vincent@datacamp.com>
2015-10-06 15:24:12 +0200

    little fix for test-that regex

--- R/test-that.R
+++ R/test-that.R
@@ -5,7 +5,7 @@ test_that <- function(descr, code) {
     expr = tree[i]
     text = as.character(expr)
     if (grepl("expect", expr)) {
-      et_pattern = "^.*(expect_\\w+\\(.*),\\s?(failure_.*\\s?=\\s?)?(.*)\\)$"
+      et_pattern = "^.*(expect_\\w+\\(.*),\\s?failure_msg(\\s?=\\s?)?(.*)\\)$"
       tw_repl = "test_what(\\1), \\3)"
       repl_text = sub(et_pattern, tw_repl, text)
       eval(parse(text=repl_text))
