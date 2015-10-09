139a9d67b505611ca4aefa579d693f238d5c5e6e
Vincent Vankrunkelsven <vincentvankrunkelsven@Vincents-MacBook-Pro.local>
2015-10-09 10:19:10 +0200

    Fixed bug with success msg inn challenge

--- R/reporter-DataCamp.R
+++ R/reporter-DataCamp.R
@@ -115,8 +115,10 @@ DataCampReporter <- setRefClass(
           all(results_df$passed[results_df$instruction_index == x])
         })
         
-        return(list(passed = tail(res_per_inst, 1), 
-                    feedback = "try again.", 
+        challenge_passed <- tail(res_per_inst, 1)
+        
+        return(list(passed = challenge_passed, 
+                    feedback = ifelse(challenge_passed, inh_success_msg, "try again."), 
                     passed_steps = head(res_per_inst, n_inst - 1)))
       } else {
         stop("Unknown report type!")
