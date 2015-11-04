16beb918ad6dab9613364555a9ae48fedd3086a4
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2015-11-04 15:19:08 +0100

    update

--- R/testwhat.R
+++ R/testwhat.R
@@ -6,7 +6,9 @@
 #' This package contains a bunch of high-level functions to test objects, function
 #' calls, output, function definition, control structures and many more.
 #' 
-#' For more information, see \url{docs.datacamp.com/teach/sct-design-r.html}
+#' For more information, see \url{docs.datacamp.com/teach/sct-design-r.html}.
+#' 
+#' For a bunch of SCT examples, see \url{www.github.com/data-camp/introduction_to_R}.
 #'
 #' @docType package
 #' @name testwhat
