7fc81ea422de1fd916dd7f216796ec62198d3f85
Vincent Vankrunkelsven <vincent@datacamp.com>
2015-12-19 20:48:32 +0100

    Last hands on ggplot checker

--- R/test-ggplot.R
+++ R/test-ggplot.R
@@ -3,7 +3,10 @@ test_ggplot <- function(index = 1,
                         student_code = get_student_code(),
                         solution_code = get_solution_code(),
                         student_env = .GlobalEnv,
-                        solution_env = get_solution_env()) {
+                        solution_env = get_solution_env(),
+                        check_data = TRUE, data_fail_msg = NULL,
+                        check_aes = TRUE, aes_fail_msg = NULL, exact_aes = FALSE,
+                        check_geom = TRUE, geom_fail_msg = NULL, exact_geom = FALSE) {
   solution_ggplot_objects <- get_ggplot_objects(solution_code, solution_env)
   
   sol_selected <- try(solution_ggplot_objects[[index]], silent = TRUE)
@@ -18,117 +21,236 @@ test_ggplot <- function(index = 1,
     test_what(fail(), feedback_msg = "You didn't define enough `ggplot` commands.")
   }
   
-  feedback <- sprintf("For ggplot command %s,", index)
+  feedback <- sprintf("In your %s `ggplot` command,", nd(index))
   
   stud_selected <- student_ggplot_objects[[index]]
   
-  # Check the data
-  sol_data <- list(base = sol_selected$data)
+  test_what(expect_false(inherits(stud_selected, "try-error")), feedback_msg = paste(feedback, "you got an error. Make sure you use the correct `ggplot` syntax. Have another look at the instructions."))
   
-  stud_data <- list(base = stud_selected$data)
-
-  test_what(expect_equal(sol_data$base, stud_data$base), feedback_msg = paste(feedback, "you didn't get the data layer correct."))
+  if (check_data) {
+    # Check the data
+    sol_data <- list(base = sol_selected$data)
+    
+    stud_data <- list(base = stud_selected$data)
   
-  # Check the mapping
-  sol_mapping <- list(base = sol_selected$mapping)
-
-  stud_mapping <- list(base = stud_selected$mapping)
-  
-  for (map in names(sol_mapping$base)) {
-    test_what(expect_false(is.null(stud_mapping$base[map][[1]])), feedback_msg = paste0(feedback, " have you mapped something on the `", map, "` aesthetic?"))
-    test_what(expect_equal(stud_mapping$base[map], sol_mapping$base[map]), feedback_msg = paste0(feedback, " have you mapped `", sol_mapping$base[map] ,"` on the `", map, "` aesthetic? Found `", stud_mapping$base[map], "`."))
+    if (!is.null(data_fail_msg)) {
+      feedback_msg <- data_fail_msg
+    } else {
+      feedback_msg <- paste(feedback, "you didn't get the data layer right.")
+    }
+    
+    test_what(expect_equal(sol_data$base, stud_data$base), feedback_msg = feedback_msg)
   }
   
-  # Check the geom layer
-  stud_layers <- stud_selected$layers
+  if (check_aes) {
+    # Check the mapping
+    sol_mapping <- list(base = sol_selected$mapping)
   
-  for (sol_layer in sol_selected$layers) {
-    found_geom_name <- FALSE
-    found_geom_with_params <- TRUE
+    stud_mapping <- list(base = stud_selected$mapping)
     
-    sol_params <- sol_layer$geom_params
-    sol_params <- c(sol_params, sol_layer$stat_params)
-    sol_params <- c(sol_params, lapply(sol_layer$mapping, function(x) structure(x, aes = TRUE)))
-    sol_params_failed <- c()
-    
-    nb_stud_layers <- length(stud_layers)
-    if (nb_stud_layers > 0) {
-      for (i in 1:nb_stud_layers) {
-        stud_layer <- stud_layers[[i]]
-        if (stud_layer$geom$objname == sol_layer$geom$objname) {
-          found_geom_name <- TRUE
-          found_geom_with_params <- TRUE
-          stud_params <- stud_layer$geom_params
-          stud_params <- c(stud_params, stud_layer$stat_params)
-          stud_params <- c(stud_params, lapply(stud_layer$mapping,function(x) structure(x, aes = TRUE)))
-          
-          for (sol_param in names(sol_params)) {
-            if (!(sol_param %in% names(stud_params))) {
-              found_geom_with_params <- FALSE
-              sol_params_failed <- c(sol_params_failed, sol_param)
-              break
-            } else {
-              sol_value <- sol_params[[sol_param]]
-              stud_value <- stud_params[[sol_param]]
-              
-              if (!all(sol_value == stud_value)) {
-                found_geom_with_params <- FALSE
-                sol_params_failed <- c(sol_params_failed, sol_param)
-                break
-              }
-            }
-          }
-          
-          if (found_geom_with_params) {
-            stud_layers[[i]] <- NULL
-            break
-          }
-        }
+    for (map in names(sol_mapping$base)) {
+      if (!is.null(aes_fail_msg)) {
+        feedback_msg <- rep_len(data_fail_msg, 3)
+      } else {
+        feedback_msg <- c(paste0(feedback, " have you mapped something on the `", map, "` aesthetic?"),
+                          paste0(feedback, " have you mapped `", sol_mapping$base[map] ,"` on the `", map, "` aesthetic? Instead, you got `", stud_mapping$base[map], "`."),
+                          paste0(feedback, " have you mapped exactly what is asked on the aesthetics layer, no more and no less?"))
+      }
+      
+      test_what(expect_false(is.null(stud_mapping$base[map][[1]])), feedback_msg = feedback_msg[1])
+      test_what(expect_equal(stud_mapping$base[map], sol_mapping$base[map]), feedback_msg = feedback_msg[2])
+      if (exact_aes) {
+        test_what(expect_equal(length(stud_mapping), length(sol_mapping)), feedback_msg = feedback_msg[3])
       }
     }
     
-    geom_base_feedback <- paste0(feedback, " have you correctly added a `geom_", sol_layer$geom$objname,"()` layer")
-    test_what(expect_true(found_geom_name), feedback_msg = paste0(geom_base_feedback, "?"))
-    param_feedback <- paste(vapply(unique(sol_params_failed), function(x) paste0(ifelse(isTRUE(attr(sol_params[[x]], "aes")), "aesthetic ", ""), "`", x, "` set to `", sol_params[[x]], "`"), character(1)), collapse = ", ")
-    test_what(expect_true(found_geom_with_params), feedback_msg = paste0(geom_base_feedback, " with ", param_feedback, "?"))
+    
+  }
+  
+  if (check_geom) {
+    # Check the geom layer
+    sol_layers <- sol_selected$layers
+    stud_layers <- stud_selected$layers
+    
+    nb_sol_layers <- length(sol_layers)
+    
+    exact_geom <- rep_len(exact_geom, nb_sol_layers)
+    
+    for (i in 1:nb_sol_layers) {
+      sol_layer <- sol_layers[[i]]
+      
+      found_geom_name <- FALSE
+      found_geom_with_params <- FALSE
+      found_geom_with_exact_params <- FALSE
+      
+      sol_params <- sol_layer$geom_params
+      sol_params <- c(sol_params, sol_layer$stat_params)
+      sol_params <- c(sol_params, lapply(sol_layer$mapping, function(x) structure(x, aes = TRUE)))
+      
+      nb_stud_layers <- length(stud_layers)
+      if (nb_stud_layers > 0) {
+        for (j in 1:nb_stud_layers) {
+          stud_layer <- stud_layers[[j]]
+          if (stud_layer$geom$objname == sol_layer$geom$objname) {
+            found_geom_name <- TRUE
+            found_params <- TRUE
+            
+            stud_params <- stud_layer$geom_params
+            stud_params <- c(stud_params, stud_layer$stat_params)
+            stud_params <- c(stud_params, lapply(stud_layer$mapping, function(x) structure(x, aes = TRUE)))
+            
+            for (sol_param in names(sol_params)) {
+              if (!(sol_param %in% names(stud_params))) {
+                found_params <- FALSE
+                break
+              } else {
+                sol_value <- sol_params[[sol_param]]
+                stud_value <- stud_params[[sol_param]]
+                
+                if (!all(sol_value == stud_value)) {
+                  found_params <- FALSE
+                  break
+                }
+              }
+            }
+            
+            if (found_params) {
+              found_geom_with_params <- TRUE
+            }
+            
+            if (found_geom_with_params && (!exact_geom[i] || length(sol_params) == length(stud_params))) {
+              found_geom_with_exact_params <- TRUE
+            }
+              
+            if (found_geom_with_exact_params) {
+              stud_layers[[j]] <- NULL
+              break
+            }
+          }
+
+        }
+      }
+      
+      if (!is.null(geom_fail_msg)) {
+        feedback_msg <- rep_len(geom_fail_msg, 3)
+      } else {
+        geom_base_feedback <- paste0(feedback, " have you correctly added a `geom_", sol_layer$geom$objname,"()` layer")
+        filtered_geom_params <- names(filter_standard_geom_params(sol_layer$geom$objname, sol_params))
+        param_strings <- vapply(filtered_geom_params, 
+                                function(x) paste0(ifelse(isTRUE(attr(sol_params[[x]], "aes")), "aesthetic ", ""), 
+                                                  "`", x, "` set to `", sol_params[[x]], "`"), character(1))
+        nb_param_strings <- length(param_strings)
+        if (nb_param_strings > 1) {
+          param_feedback <- paste0(paste(param_strings[1:(nb_param_strings-1)], collapse = ", "), " and ", param_strings[nb_param_strings])
+        } else {
+          param_feedback <- param_strings
+        }
+        feedback_msg <- c(paste0(geom_base_feedback, "?"),
+                          paste0(geom_base_feedback, " with ", param_feedback, "?"),
+                          paste0(geom_base_feedback, " with ", param_feedback, "?", " It seems like you have defined too much attributes or aesthetics for this geom."))
+        
+      }
+        
+      test_what(expect_true(found_geom_name), feedback_msg = feedback_msg[1])
+      test_what(expect_true(found_geom_with_params), feedback_msg = feedback_msg[2])
+      test_what(expect_true(found_geom_with_exact_params), feedback_msg = feedback_msg[3])
+    }
   }
 }
 
-get_ggplot_objects <- function(code, envir) {
-  return(lapply(get_ggplot_commands(code), function(x) eval(x, envir)))
+nd <- function(number) {
+  switch(number, "1" = "first", "2" = "second", "3" = "third", 
+                 "4" = "fourth", "5" = "fifth", "6" = "sixth", 
+                 "7" = "seventh", "8" = "eighth", "9" = "ninth", 
+                 "10" = "tenth")
 }
 
-get_ggplot_commands <- function(code) {
+filter_standard_geom_params <- function(geom_call, params) {
+  standard_layer <- eval(call(paste0("geom_",geom_call)))
+  standard_params <- standard_layer$geom_params
+  standard_params <- c(standard_params, standard_layer$stat_params)
+  standard_params <- c(standard_params, lapply(standard_layer$mapping,function(x) structure(x, aes = TRUE)))
+  ov <- intersect(names(params), names(standard_params))
+  eq <- mapply('==', standard_params[ov], params[ov])
+  if (any(eq)) {
+    params[[names(eq[eq])]] <- NULL
+  } 
+  return(params)
+}
+
+get_ggplot_objects <- function(code, envir) {
+  ggplot_env <- new.env()
+  return(lapply(get_ggplot_commands(code, ggplot_env), function(x) try(eval(x, envir), silent = TRUE) ))
+}
+
+get_ggplot_commands <- function(code, envir) {
   parsed <- try(parse(text = code), silent = TRUE)
   
   if (inherits(parsed, "try-error")) {
     return(list())
   }
   
-  extracted <- lapply(parsed, extract_ggplot_command)
+  extracted <- lapply(parsed, extract_ggplot_command, envir = envir)
   return(extracted[!as.logical(vapply(extracted, is.null, logical(1)))])
 }
 
-extract_ggplot_command <- function(code) {
-  if (is_ggplot_command(code)) {
-    return(code)
+extract_ggplot_command <- function(code, envir) {
+  if (is_ggplot_command(code, envir)) {
+    return(replace_saved_ggplot_commands(code, envir))
   } else if (is.name(code)) {
     return(NULL)
   } else if (code[[1]] == "=" || code[[1]] == "<-") {
-    return(extract_ggplot_command(code[[3]]))
+    code_is_ggplot_command <- is_ggplot_command(code[[3]], envir)
+    if (code_is_ggplot_command || is_geom_command(code[[3]], envir)) {
+      extract_def <- replace_saved_ggplot_commands(code[[3]], envir)
+      assign(as.character(code[[2]]), extract_def, envir = envir)
+      if (isTRUE(code_is_ggplot_command)) {
+        return(extract_def)
+      } else {
+        return(NULL)
+      }
+    } else {
+      return(NULL)
+    }
   } else {
     return(NULL)
   }
 }
 
-is_ggplot_command <- function(code) {
-  if (!is.call(code)) {
-    return(FALSE)
+replace_saved_ggplot_commands <- function(code, envir) {
+  len <- length(code)
+  if (len > 1) {
+    for (i in 1:len) {
+      if (exists(as.character(code[[i]]), envir = envir, inherits = FALSE)) {
+        code[[i]] = get(as.character(code[[i]]), envir = envir, inherits = FALSE)  
+      }
+    }
+  } else {
+    if (exists(as.character(code), envir = envir, inherits = FALSE)) {
+      code =  get(as.character(code), envir = envir, inherits = FALSE) 
+    }
+  }
+  return(code)
+}
+
+is_ggplot_command <- function(code, envir) {
+  if (is.name(code)) {
+    get_command <- try(get(as.character(code), envir = envir, inherits = FALSE))
+    return(ifelse(inherits(get_command, "try-error"), FALSE, is_ggplot_command(get_command, envir)))
   } else if (code[[1]] == "ggplot") {
     return(TRUE)
   } else if (code[[1]] == "+") {
-    return(is_ggplot_command(code[[2]]))
+    return(is_ggplot_command(code[[2]], envir))
   } else {
     return(FALSE)
+  }
+}
+
+is_geom_command <- function(code, envir) {
+  if (is.name(code)) {
+    get_command <- try(get(as.character(code), envir = envir, inherits = FALSE))
+    return(ifelse(inherits(get_command, "try-error"), FALSE, is_geom_command(get_command, envir)))
+  } else {
+    return(isTRUE(grepl("^geom_", code[[1]])))
   }
 }
--- inst/scenarios/test-test-ggplot.R
+++ inst/scenarios/test-test-ggplot.R
@@ -5,16 +5,16 @@ scen <- list(
     type = "NormalExercise",
     pre_exercise_code = "library(ggplot2)",
     student = "
-ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_smooth(se = FALSE) + geom_point(aes(alpha = disp), size = 5) + geom_point( size = 5, alpha = 0.3)
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_smooth(se = FALSE) + geom_point(size = 5, aes(alpha = disp)) + geom_point(alpha = 0.5, size=3)
     ",
     solution = "
-ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_point(alpha = 0.3, size = 5) + geom_point(size = 5, aes(alpha = disp)) + geom_smooth(se = FALSE)
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_point(alpha = 0.5) + geom_point(size = 5, aes(alpha = disp)) + geom_smooth(se = FALSE)
     ",
     pass = list(
       test_correct_data = list(
         long = "test succeeds if command has the correct data",
         sct = "
-test_ggplot()
+test_ggplot(exact_geom = c(TRUE, FALSE, FALSE))
         "
       )
     )
