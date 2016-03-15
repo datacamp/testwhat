e61490782333afffca3d3815e5e2c0d9c15a1726
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-03-15 14:22:03 +0100

    uncomment some stuff

--- R/utils-rstudio.r
+++ R/utils-rstudio.r
@@ -1,26 +1,18 @@
 create_student_pd <- function(student_code = NULL) {
-  #student_pd <- tw$get("student_pd")
-  #if(!is.null(student_pd)) return(student_pd)
-  
   if(is.null(student_code)) {
     stop("The student_code argument cannot be empty")
   }
   student_code_parts = get_clean_lines(code = student_code)
   stud = lapply(student_code_parts, function(x) getParseData(parse(text = x, keep.source = TRUE)))
-  #tw$set(student_pd = stud)
   return(stud)
 }
 
 create_solution_pd <- function(solution_code = NULL) {
-  #solution_pd <- tw$get("solution_pd")
-  #if(!is.null(solution_pd)) return(solution_pd)
-  
   if(is.null(solution_code)) {
     stop("The solution_code argument cannot be empty")
   }
   solution_code_parts = get_clean_lines(code = solution_code)
   sol = lapply(solution_code_parts, function(x) getParseData(parse(text = x, keep.source = TRUE)))
-  #tw$set(solution_pd = sol)
   return(sol)
 }
 
@@ -41,11 +33,11 @@ get_expression <- function(id, pd) {
 
 # From all parsedata for a set of commands, return only the parse data linked to a single command.
 get_single_pd = function(index, pd, incorrect_number_of_calls_msg = NULL) {
-  #ok <- testwhat:::test_sufficient_length(stud = pd, index = index, incorrect_number_of_calls_msg = incorrect_number_of_calls_msg)
-  #if(ok) {
+  ok <- testwhat:::test_sufficient_length(stud = pd, index = index, incorrect_number_of_calls_msg = incorrect_number_of_calls_msg)
+  if(ok) {
     return(pd[[index]])
-  #} else {
-  #  return(NULL)
-  #}
+  } else {
+    return(NULL)
+  }
 }
 
