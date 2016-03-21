81a999d43943c9f6b0f3d32ef66fff7a78b9fafb
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-03-21 11:06:27 +0100

    rewrite test_mc

--- R/test-mc.R
+++ R/test-mc.R
@@ -4,7 +4,7 @@
 #' This code expects the DM.result variable to be defined by the DataCamp frontend. 
 #' There is need to define the success_msg seperately, since it is defined inside the function.
 #'
-#' @param correct number of the correct answer
+#' @param correct number of the correct answer (or vector of numbers, if several options are fine)
 #' @param no_selection_msg feedback message in case the student did not select an answer.
 #' @param feedback_msgs vector of feedback messages for both the incorrect exercises as the correct exercise.
 #' Order the messages according to how they are listed in the instructions. For example, if there are four options,
@@ -26,37 +26,23 @@
 #' }
 #'
 #' @export
-test_mc <- function(correct = NULL, no_selection_msg = NULL, feedback_msgs = NULL) {
-  
-  result = get("DM.result", envir = globalenv())
+test_mc <- function(correct, no_selection_msg = NULL, feedback_msgs = NULL) {
   init_tags(fun = "test_mc")
   
-  if(is.null(correct)) {
-    stop("argument 'correct' can not be empty")
-  }
-
-  #default messages:
+  # see if DM.result exists
   if(is.null(no_selection_msg)) {
     no_selection_msg <- "Please select one of the options!"
   }
-  if(is.null(feedback_msgs)) {
-    incorrect_msgs <- rep("Your answer is incorrect. Try again.",20) #20 hardcoded, should never be more..
-    default_success_msg = "Very good! Continue to the next exercise."
-    feedback_msgs = c(incorrect_msgs[1:correct-1],default_success_msg,incorrect_msgs[correct:length(incorrect_msgs)])
+  
+  test_what(exists("DM.result", envir = globalenv()), no_selection_msg)
+  result <- get("DM.result", envir = globalenv())
+  
+  # see if result is correct
+  if(!is.null(feedback_msgs) && is.na(feedback_msgs[result])) {
+    stop("There is no feedback message available for this user input! Make sure you define enough feedback messages.")
   }
   
-  if(is.na(feedback_msgs[result])) {
-    stop("There is no feedback message available for this user input! Make sure you define enough feedback messages")
-  }
-  
-  defined <- exists("result")
-  test_what(expect_true(defined), no_selection_msg)
-  if(defined) {
-    if(!is.numeric(result)) {
-      stop("There is something wrong with the result set by the backend.")
-    }
-    test_what(expect_identical(correct,result), feedback_msgs[result])
-  }
+  test_what(expect_true(result %in% correct), feedback_msg = ifelse(is.null(feedback_msgs), "Your answer is incorrect. Try again.", feedback_msgs[result]))
 
-  success_msg(feedback_msgs[correct])
+  success_msg(ifelse(is.null(feedback_msgs), "Very good! Continue to the next exercise.", feedback_msgs[correct]))
 }
--- tests/testthat/test-test-mc.R
+++ tests/testthat/test-test-mc.R
@@ -1,16 +1,42 @@
 context("test_mc")
 source("helpers.R")
 
-test_that("test_mc works works", {
+test_that("test_mc works", {
   lst <- list()
   lst$DC_TYPE <- "MultipleChoiceExercise"
-  lst$DC_CODE <- "DM.result <- 1"
-  
-  lst$DC_SCT <- "test_mc(1, feedback_msgs = c('this is the CORRECT answer', 'this is the WRONG answer'))"
-  output <- test_it(lst)
-  passes(output)
+  lst$DC_CODE <- "DM.result <- 2"
   
   lst$DC_SCT <- "test_mc(2, feedback_msgs = c('this is the WRONG answer', 'this is the CORRECT answer'))"
   output <- test_it(lst)
+  passes(output, mess_patt = "this is the CORRECT answer")
+  
+  lst$DC_SCT <- "test_mc(2)"
+  output <- test_it(lst)
+  passes(output)
+  
+  lst$DC_SCT <- "test_mc(1, feedback_msgs = c('this is the CORRECT answer', 'this is the WRONG answer'))"
+  output <- test_it(lst)
   fails(output, mess_patt = "this is the WRONG answer")
+  
+  lst$DC_SCT <- "test_mc(1)"
+  output <- test_it(lst)
+  fails(output)
+  
+  lst$DC_SCT <- "test_mc(1, feedback_msgs = c('not enugh messages'))"
+  output <- test_it(lst)
+  error(output)
+  
+  lst <- list()
+  lst$DC_TYPE <- "MultipleChoiceExercise"
+  lst$DC_CODE <- "not.DM.result <- 12"
+  lst$DC_SCT <- "test_mc(1)"
+  output <- test_it(lst)
+  error(output)
+  
+  lst <- list()
+  lst$DC_TYPE <- "MultipleChoiceExercise"
+  lst$DC_CODE <- "DM.result <- 2"
+  lst$DC_SCT <- "test_mc(c(1, 2))"
+  output <- test_it(lst)
+  passes(output)
 })
