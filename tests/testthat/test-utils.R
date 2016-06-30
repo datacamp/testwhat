c9b65b21d2bbcdac78309e84e2b7ef433e31a307
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-06-30 13:06:06 -0700

    add tests on accessors

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,18 @@
+context("utils")
+
+test_that("accessor works", {
+  tw <- tw_accessors()
+  tw$initialize(data = list(a = 2, b = 3, c = 4))
+  expect_equal(tw$get("a"), 2)
+  expect_equal(tw$get("b"), 3)
+  expect_equal(tw$get("c"), 4)
+  expect_equal(tw$get("d"), NULL)
+  tw$set(d = 5)
+  expect_equal(tw$get("a"), 2)
+  expect_equal(tw$get("b"), 3)
+  expect_equal(tw$get("c"), 4)
+  expect_equal(tw$get("d"), 5)
+  tw$clear()
+  expect_equal(tw$get(), list())
+  expect_equal(tw$get("a"), NULL)
+})
