74e77f89fbd93ebcc362c7675e74b1ab67d4c441
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-07-04 12:07:23 -0700

    import unexported ggvis functions, otherwise path with no end...

--- R/rstudio-test-props.r
+++ R/rstudio-test-props.r
@@ -165,83 +165,14 @@ get_all_props <- function(fun, call) {
   }
 }
 
-## Functions below are copied from the ggvis package
-
 #' @importFrom lazyeval lazy_dots
-props <- function (..., .props = NULL, inherit = TRUE, env = parent.frame()) {
-  check_empty_args()
+props <- function(..., .props = NULL, inherit = TRUE, env = parent.frame()) {
+  ggvis:::check_empty_args()
   args <- pluck(lazyeval::lazy_dots(...), "expr")
-  all <- args_to_props(c(args, .props), env)
+  all <- ggvis:::args_to_props(c(args, .props), env)
   structure(all, inherit = inherit, class = "ggvis_props")
 }
 
-pluck <- function (x, name) {
+pluck <- function(x, name) {
   lapply(x, `[[`, name)
 }
-
-check_empty_args <- function () {
-  call <- sys.call(-1)
-  args <- as.list(call[-1])
-  is_missing <- function(x) identical(x, quote(expr = ))
-  missing <- vapply(args, is_missing, logical(1))
-  if (!any(missing))
-    return(invisible(TRUE))
-  stop("Extra comma at position", if (sum(missing) > 1)
-    "s", " ", paste0(which(missing), collapse = ", "), " in call to ",
-    as.character(call[[1]]), "()", call. = FALSE)
-}
-
-args_to_props <- function (args, env) {
-  expr_to_prop <- function(name, expr, scale = NULL) {
-    name <- strsplit(name, ".", fixed = TRUE)[[1]]
-    property <- name[1]
-    event <- if (length(name) > 1)
-      name[2]
-    else NULL
-    val <- eval(expr, env)
-    prop(property, val, scale = scale, event = event, label = as.character(val))
-  }
-  prop_full_name <- function(p) {
-    paste(c(p$property, p$event), collapse = ".")
-  }
-  arg_names <- names2(args)
-  named_args <- args[arg_names != ""]
-  unnamed_args <- args[arg_names == ""]
-  named_args <- Map(named_args, names(named_args),
-                    f = function(x, name) expr_to_prop(name, x, scale = TRUE))
-  unnamed_args <- lapply(unnamed_args, function(x) {
-    if (uses_colon_equals(x)) {
-      expr_to_prop(deparse(x[[2]]), x[[3]], scale = FALSE)
-    }
-    else {
-      eval(x, env)
-    }
-  })
-  is_prop <- vapply(unnamed_args, is.prop, logical(1))
-  unnamed_props <- unnamed_args[is_prop]
-  unnamed_values <- unnamed_args[!is_prop]
-  names(named_args) <- vapply(named_args, prop_full_name, character(1))
-  names(unnamed_props) <- vapply(unnamed_props, prop_full_name, character(1))
-  missing_names <- setdiff(c("x.update", "y.update"),
-                           c(names(named_args), names(unnamed_props)))
-  if (length(unnamed_values) > length(missing_names)) {
-    stop("Too many unnamed properties (can only have x and y)", call. = FALSE)
-  }
-  names(unnamed_values) <- missing_names[seq_along(unnamed_values)]
-  unnamed_values <- Map(unnamed_values, names(unnamed_values),
-                        f = function(x, name) expr_to_prop(name, x, scale = TRUE))
-  c(named_args, unnamed_props, unnamed_values)
-}
-
-uses_colon_equals <- function (x) {
-  is.call(x) && identical(x[[1]], quote(`:=`))
-}
-
-names2 <- function (x) {
-  names(x) %||% rep("", length(x))
-}
-
-`%||%` <- function (a, b) {
-  if (!is.null(a)) a else b
-}
-
