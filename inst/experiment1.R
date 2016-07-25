0d9f215860a2789c62708848cbd56d86d1e52b37
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-07-25 12:14:30 +0200

    undo again

--- inst/experiment1.R
+++ inst/experiment1.R
@@ -1,15 +1,3 @@
-# 
-# init_output <- rjson::fromJSON(RBackend::execute(rjson::toJSON(list(DC_COMMAND = "init", 
-#                                             DC_PEC = "", 
-#                                             DC_SOLUTION = "print(TRUE)",
-#                                             DC_SCT = "test_function('print', args = 'x')",
-#                                             DC_TYPE = "NormalExercise",
-#                                             DC_ECHO = TRUE))))
-# submit_output <- rjson::fromJSON(RBackend::execute(rjson::toJSON(list(DC_COMMAND = "submit", 
-#                                               DC_TYPE = "NormalExercise",
-#                                             DC_CODE = "print(list('test', 'test2'))"))))
-# print(submit_output)
-
 library(magrittr)
 fun1 <- function(x) {
   print("this is fun 1")
