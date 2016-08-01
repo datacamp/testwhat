7896bcde644fee084334cb73e3554ade4f435bf6
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-08-01 09:19:17 +0200

    reflow

--- R/rstudio-test-chunk-options.R
+++ R/rstudio-test-chunk-options.R
@@ -1,14 +1,17 @@
-#' Test whether the student defined the correct chunk options (R Markdown exercises)
-#'
-#' Test whether the student defined the correct chunk options in an R Markdown exercise
-#'
+#' Test whether the student defined the correct chunk options (R Markdown
+#' exercises)
+#' 
+#' Test whether the student defined the correct chunk options in an R Markdown
+#' exercise
+#' 
 #' This test can only be called inside a test_rmd_group() call!
-#'
+#' 
 #' @param options  Set of options
-#' @param allow_extra  whether or not the definition of additional options is accepted (default TRUE)
+#' @param allow_extra  whether or not the definition of additional options is
+#'   accepted (default TRUE)
 #' @param not_called_msg feedback message if option was not specified
 #' @param incorrect_msg  feedback message if option was incorrectly set
-#' 
+#'   
 #' @export
 test_chunk_options <- function(options = NULL,
                                allow_extra = TRUE,
