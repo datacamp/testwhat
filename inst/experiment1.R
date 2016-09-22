ff225e5c8eab749645582e91b8b8c65af2a6123d
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-09-22 16:38:38 +0200

    update experiment file

--- inst/experiment1.R
+++ inst/experiment1.R
@@ -1,17 +1,13 @@
-library(magrittr)
-fun1 <- function(x) {
-  print("this is fun 1")
-  x
-}
-
-fun2 <- function(x) {
-  print("this is fun2")
-  x
-}
-
-fun3 <- function(x) {
-  print("this is fun3")
-  x
-}
-
-fun1(3) %>% fun2() %>% fun3()
+library(RBackend)
+lst <- list(DC_TYPE = "NormalExercise",
+            DC_ECHO = TRUE,
+            DC_PEC = "",
+            DC_SOLUTION = "x <- mean(1:3)\ny <- mean(c(1:3, NA), na.rm = TRUE)",
+            DC_COMMAND = "init",
+            DC_SCT = "test_function('mean', args = 'x', index = 1)\ntest_function('mean', args = 'x', index = 2)\ntest_function('mean', args = 'x', index = 3)")
+output <- rjson::fromJSON(execute(rjson::toJSON(lst)))
+lst <- list(DC_TYPE = "NormalExercise",
+            DC_ECHO = TRUE,
+            DC_CODE = "x <- mean(1:3)\ny <- mean(c(1:3, NA), na.rm = TRUE)",
+            DC_COMMAND = "submit")
+output <- rjson::fromJSON(execute(rjson::toJSON(lst)))
