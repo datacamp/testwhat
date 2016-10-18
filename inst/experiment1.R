a8ed3566b49f0b02199d4a7a3e3628c7d5dde6cb
Filip Schouwenaars <filip@datacamp.com>
2016-10-18 10:41:16 +0200

    update experiment

--- inst/experiment1.R
+++ inst/experiment1.R
@@ -2,12 +2,11 @@ library(RBackend)
 lst <- list(DC_TYPE = "NormalExercise",
             DC_ECHO = TRUE,
             DC_PEC = "",
-            DC_SOLUTION = "x <- mean(1:3)\ny <- mean(c(1:3, NA), na.rm = TRUE)",
+            DC_SOLUTION = "x <- 10",
             DC_COMMAND = "init",
-            DC_SCT = "test_function('mean', args = 'x', index = 1)\ntest_function('mean', args = 'x', index = 2)\ntest_function('mean', args = 'x', index = 3)")
+            DC_SCT = "test_object('x')")
 output <- rjson::fromJSON(execute(rjson::toJSON(lst)))
 lst <- list(DC_TYPE = "NormalExercise",
-            DC_ECHO = TRUE,
-            DC_CODE = "x <- mean(1:3)\ny <- mean(c(1:3, NA), na.rm = TRUE)",
+            DC_CODE = "x <- 8",
             DC_COMMAND = "submit")
 output <- rjson::fromJSON(execute(rjson::toJSON(lst)))
