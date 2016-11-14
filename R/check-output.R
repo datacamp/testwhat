72a4b7f3b06bb5cf3a5f32b0dbf686d165614bfe
Filip Schouwenaars <filip@datacamp.com>
2016-11-14 15:08:18 +0100

    remove extra spaces

--- R/check-output.R
+++ R/check-output.R
@@ -1,17 +1,17 @@
 #' Check whether the student printed something to the console
-#' 
+#'
 #' Check the output of the submission to see if it contains certain elements.
-#' 
+#'
 #' With \code{check_output}, you can simply specify a regular expression or
 #' pattern (depending on the value of \code{fixed}) that is looked for in the
 #' student's output. By default, regular output, messages, warnings and errors
 #' are considered.
-#' 
+#'
 #' With \code{test_output_contains} and \code{check_output_expr} you can pass an
 #' expression, that is executed and whose output is compared to the output the
 #' student generated. If the generated output is found in the student's output,
 #' the check passes. By default, only regular output is considered.
-#' 
+#'
 #' @param state the state to start from
 #' @param regex the regular expression or pattern to look for
 #' @param fixed if fixed is TRUE, \code{regex} will be sought for 'as is' in the
@@ -29,30 +29,30 @@
 #' @param ... S3 stuff
 #' @param append Whether or not to append the feedback to feedback built in
 #'   previous states
-#'   
+#'
 #' @param expr The expression (as string) for which the output should be in the
 #'   student's console output.
 #' @param incorrect_msg Custom message in case the output of the expression
 #'   wasn't found often enough in the student's output.
-#'   
+#'
 #' @examples
 #' \dontrun{
 #' # Example 1
 #' mtcars
-#' 
+#'
 #' # SCT option 1
 #' test_output_contains("mtcars")
-#' 
+#'
 #' # SCT option 2
 #' ex() %>% check_output_expr("mtcars")
-#' 
+#'
 #' # Example 2
 #' print("hello!")
-#' 
+#'
 #' # SCT (robust)
 #' ex() %>% check_output("[H|h]ello\\!*")
 #' }
-#' 
+#'
 #' @name test_output
 
 
@@ -84,14 +84,14 @@ test_output_contains <- function(expr, times = 1, incorrect_msg = NULL) {
 
 #' @rdname test_output
 #' @export
-check_output_expr <- function(state, expr, times = 1,  missing_msg = NULL, append = TRUE) {
-  
+check_output_expr <- function(state, expr, times = 1, missing_msg = NULL, append = TRUE) {
+
   expr_output <- try(capture.output(base::eval(parse(text = expr), envir = ex()$get("student_env"))), silent = TRUE)
   if (inherits(expr_output, "try-error")) {
     expr_output <- ""
   }
   expr_output <- paste(expr_output, collapse = "\n")
-  
+
   regex_state <- RegexState$new(state)
   regex_state$add_details(type = "output",
                           case = "expr",
@@ -100,7 +100,7 @@ check_output_expr <- function(state, expr, times = 1,  missing_msg = NULL, appen
                           message = missing_msg,
                           append = append,
                           pd = NULL)
-  
+
   console_output <- trim_output(convert_output_list(state$get("output_list"), output_only = TRUE))
   num_hits <- get_num_hits(regex = trim_output(expr_output), x = console_output, fixed = TRUE)
   check_that(is_gte(num_hits, times), feedback = regex_state$details)
@@ -135,7 +135,7 @@ get_num_hits <- function(regex, x, fixed) {
       return(0)
     } else {
       return(sum(counts))
-    }  
+    }
   }
 }
 
