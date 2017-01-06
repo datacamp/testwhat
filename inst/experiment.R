d4c25d1f19bff8bf7c970703b9f10ee1f38ceeab
Filip Schouwenaars <filip@datacamp.com>
2017-01-06 19:05:54 +0100

    make experimentation easier

---
+++ inst/experiment.R
@@ -0,0 +1,18 @@
+# install packages
+install <- FALSE
+auth_token <- "put_auth_token_here"
+if (install) {
+  devtools::install_github("datacamp/r-completion", auth_token = auth_token)
+  devtools::install_github("datacamp/r-backend", auth_token = auth_token, dependencies = FALSE)
+  devtools::install_github("datacamp/testwhat")
+}
+
+# Source all the helper functions you need
+source(file.path(system.file(package = "testwhat"), "tests", "testthat", "helper.R"))
+
+lst <- list(DC_PEC = "y <- 6", # set to "" if not specified
+            DC_SOLUTION = "x <- 10",
+            DC_CODE = "x <- 10",
+            DC_SCT = "test_object('x')")
+
+get_sct_payload(test_it(lst))[c('correct', 'message')]
