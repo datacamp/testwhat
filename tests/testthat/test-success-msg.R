0942b58045506239b8fb56b4b34ebe8adfab5e34
Michael Chow <machow@princeton.edu>
2017-02-10 09:36:13 +0100

    comment on unit test

--- tests/testthat/test-success-msg.R
+++ tests/testthat/test-success-msg.R
@@ -6,7 +6,7 @@ test_that("success_msg", {
   passes(output, mess_patt = "This is correct")
   lst <- list(DC_SCT = "success_msg('this is correct', praise = TRUE)")
   output <- test_it(lst)
-  passes(output, mess_patt = "You are")
+  passes(output, mess_patt = "You are")             # verify random praise
   passes(output, mess_patt = "This is correct")
 })
   
