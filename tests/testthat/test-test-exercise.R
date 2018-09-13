e772c7dfa054f4274398199a845b00217e0a8e74
Filip Schouwenaars <filip@datacamp.com>
2018-09-13 10:07:57 +0200

    feat(auto-error-check): error checking consistent with pythonwhat
    - If SCT passes, but script throws exercise: still fail SCT
    - Expose additional argument on test_exercise to allow for errors
    - Update behavior accordingly
    - Fix triple r's in error messages
    
    Closes #165, #201

--- tests/testthat/test-test-exercise.R
+++ tests/testthat/test-test-exercise.R
@@ -2,34 +2,61 @@ context("test_exercise")
 
 test_that("test_exercise works properly", {
   
-  run_test_exercise <- function(stu_code, ..., sct) {
+  run_test_exercise <- function(stu_code, stu_env, sct = '', output_list = list(), allow_errors=FALSE) {
     test_exercise(
       sct = sct,
       ex_type = "NormalExercise", 
       pec = "",
       student_code = stu_code,
       solution_code = "x <- 4",
-      student_env = list2env(list(...), parent = globalenv()),
+      student_env = list2env(stu_env, parent = globalenv()),
       solution_env = list2env(list(x = 4), parent = globalenv()),
-      output_list = list(),
+      output_list = output_list,
+      allow_errors = allow_errors,
       seed = 42
     )  
   }
   
-  expect_true(run_test_exercise("x <- 4", x = 4, sct = "test_object('x')")$correct)
+  # sct fails
+  res <- run_test_exercise(stu_code = "x <- 5",
+                           stu_env = list(x = 5),
+                           sct = "ex() %>% check_object('x') %>% check_equal()")
+  expect_false(res$correct)
   
-  expect_false(run_test_exercise("x <- 5", x = 5, sct = "test_object('x')")$correct)
+  # sct fails with custom message
+  res <- run_test_exercise(stu_code = "x <- 5",
+                           stu_env = list(x = 5),
+                           sct = "ex() %>% check_object('x') %>% check_equal(incorrect_msg='abc')")
+  expect_false(res$correct)
+  expect_true(grepl('Abc', res$message))
+  
+  # sct fails with custom message outside of function
+  res <- run_test_exercise(stu_code = "x <- 5",
+                           stu_env = list(x = 5),
+                           sct = "msg <- 'abc'\nex() %>% check_object('x') %>% check_equal(incorrect_msg = msg)")
+  expect_false(res$correct)
+  expect_true(grepl('Abc', res$message))
+  
+  # sct passes, but contains an error
+  res <- run_test_exercise(stu_code = "x <- y",
+                           stu_env = list(),
+                           sct = '',
+                           output_list = list(list(type = 'r-error', payload = 'undefined variable')))
+  expect_false(res$correct)
+  expect_true(grepl('Your code contains an error that you should fix', res$message))
 
-  # message outside of function
-  sct <- "msg <- 'incorrect_obj'\ntest_object('x', incorrect_msg = msg)"
-  res <- run_test_exercise("x <- 5", x = 5, sct = sct)
-  expect_false(res$correct)
-  expect_true(grepl("Incorrect_obj", res$message))
+  # sct passes, contains an error, but errors are allowed: should be fine
+  res <- run_test_exercise(stu_code = "x <- y",
+                           stu_env = list(),
+                           sct = '',
+                           output_list = list(list(type = 'r-error', payload = 'undefined variable')),
+                           allow_errors = TRUE)
+  expect_true(res$correct)
   
-  # message outside of construct
-  sct <- "msg <- 'incorrect_obj'\ntest_correct(test_object('x', incorrect_msg = msg), test_student_typed('x'))"
-  res <- run_test_exercise("x <- 5", x = 5, sct = sct)
-  expect_false(res$correct)
-  expect_true(grepl("Incorrect_obj", res$message))
+  # sct passes
+  res <- run_test_exercise(stu_code = "x <- 4",
+                           stu_env = list(x = 4),
+                           sct = "ex() %>% check_object('x') %>% check_equal()")
+  expect_true(res$correct)
 })
 
